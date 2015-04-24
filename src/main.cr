c84cca995d9224e99c255756f1d5538dc85250c4
a5i <Ap0ph1s@users.noreply.github.com>
2015-04-24 19:19:26 -0400

    release 0.0.1

--- src/main.cr
+++ src/main.cr
@@ -3,36 +3,38 @@
 require "http/client"
 require "json"
 
+#sen = {} of String => Array
+
 class GithubRepo
-  json_mapping({
-    name: { type: String },
-    watchers_count: { type: Int32 },
-    description: { type: String, nilable: true }
-  })
+	json_mapping({
+  	  name: { type: String },
+  	  watchers_count: { type: Int32 },
+  	  description: { type: String, nilable: true }
+	})
 end
 
 class Compare
-    json_mapping({
-    name: { type: String },
-    description: { type: String }
-    })
+	json_mapping({
+	    name: { type: String },
+	    description: { type: String }
+	})
 end
 
 class GithubRepos
-  json_mapping({
-    total_count: { type: Int32 },
-    items: { type: Array(GithubRepo) }
-  })
+	json_mapping({
+	    total_count: { type: Int32 },
+	    items: { type: Array(GithubRepo) }
+	})
 
-  def items
-      @items.sort_by(&.watchers_count).reverse
-  end
+def items
+	@items.sort_by(&.watchers_count).reverse
+	end
 end
 
 def headers
-  headers = HTTP::Headers.new
-  headers["User-Agent"] = "crystalshards"
-  headers
+	headers = HTTP::Headers.new
+	headers["User-Agent"] = "crystalshards"
+	headers
 end
 
 client = HTTP::Client.new("api.github.com", 443, true)
@@ -41,18 +43,34 @@ total = GithubRepos.from_json(response.body).total_count
 puts total
 par = GithubRepos.from_json(response.body).items.map {|repo| [repo.name, repo.description] }.to_h.to_a
 
-if File.exists?("list.json") == false 
+if File.exists?("list.json") == false
 	File.open("list.json", "w") { |f|
-		f << par.to_h.to_json
-		puts "File saved"
+	    f << par.to_h.to_json
+	    puts "File saved"
 	}
 elsif
-    par1 = Hash(String, String).from_json File.read("list.json")
-    res = par-par1.to_a
-    if res.length == 0
-	puts "No"
-    elsif
-	puts res
-    end
+	par1 = Hash(String, String).from_json File.read("list.json")
+	pH = par-par1.to_a
+	if pH.length == 0
+		File.open("list.json", "w") { |f|
+		    f << ""
+		    puts "No new projects"
+	}
+	elsif
+		res = pH
+		#sen["tweet"] = res[0]
+		File.open("tweet.json", "w") { |f|
+		    res[0].each do |k, v|
+		    	f << "#{k} - #{v}"
+		    end
+		    puts "New project recieved! Tweeting..."
+		}
+		`ruby twit.rb`
+		puts "Tweeted!"
+		File.open("list.json", "w") { |f|
+		    f << par.to_h.to_json
+		    puts "File Updated"
+		}
+	end
 end
-	
+
